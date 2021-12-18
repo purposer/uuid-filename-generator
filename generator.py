4fa49c71ccb893ceb323252481d8ff4cca35a393
Ismael <ismaelbarajas.dev@gmail.com>
2021-12-18 12:19:57 -0800

    now renames using uuid5

--- generator.py
+++ generator.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
-from random import randint
 import os
-
-
-def random_with_N_digits(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
-    return randint(range_start, range_end)
+import uuid
 
 
 def rename_files():
     for filename in os.listdir("./img"):
         ext = os.path.splitext(filename)[1]
-        os.rename(f"./img/{filename}", f"./img/{random_with_N_digits(8)}{ext}")
+        if ext == ".txt":
+            continue
+        id = uuid.uuid5(uuid.NAMESPACE_OID, filename)
+        os.rename(f"./img/{filename}", f"./img/{id}{ext}")
 
 
 rename_files()
